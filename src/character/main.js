93e3aeefb6731fbdffd53a8b480d36a57605d1ec
Ecssooo <alexandre.lejeune9@gmail.com>
2024-03-15 10:21:48 +0100

    v1

---
+++ src/character/main.js
@@ -0,0 +1,26 @@
+function fetchCharacter() {
+  let url = window.location.search;
+  let slug = new URLSearchParams(url).get("slug");
+  return fetch("https://hp-api.lainocs.fr/characters/" + slug).then(
+    (response) => response.json()
+  );
+}
+async function displayCharacter() {
+  const data = await fetchCharacter();
+  console.log(data);
+  document.querySelector("#character_card").innerHTML = `
+    <a id="button_back" href="../accueil/index.html">Back</a>
+      <div class="character_card">
+      <img class="character_card img" src="${data.image}" alt="${data.name}">
+        <div id="character_card info">  
+            <h2>${data.name}</h2>
+            <ul id="ulinfo">
+            <li>${data.house}</li>
+                <li>${data.role}</li>
+                <li>${data.blood}</li>
+            </ul>
+        </div>
+      </div>
+    `;
+}
+displayCharacter();
